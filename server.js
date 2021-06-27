95fdb18960dc22fb37c0bcde67e0babde9b1a6dc
Srdjan <stojanovicsrdjan27@gmail.com>
2021-06-28 01:08:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: true })); //
 
 
 app.use(session({
-    secret: 'keyboard cat',
+    secret: 'secret!@#$',
     resave: true,
     saveUninitialized: true
 }));
